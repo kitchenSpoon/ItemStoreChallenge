646ca4cbe65f7fec38509a9fbd60a1d6a1851d5c
JackLian <hljw4@Hotmail.com>
2016-06-13 18:31:16 +1000

    Implement functions to select photos and to validate user input

--- ItemStoreChallenge/ItemStoreChallenge/ItemStoreCreateItemViewController.swift
+++ ItemStoreChallenge/ItemStoreChallenge/ItemStoreCreateItemViewController.swift
@@ -10,6 +10,71 @@ import Foundation
 
 class ItemStoreCreateItemViewController: UIViewController
 {
-    var server: ItemStoreServer?
     
+    @IBOutlet weak var selectedImageImageView: UIImageView!
+    @IBOutlet weak var nameField: UITextField!
+    
+    var server: ItemStoreServer?
+    var image: UIImage? = nil
+    
+    
+    @IBAction func didClickAddImageButton(sender: UIButton)
+    {
+        pickPhoto()
+    }
+    
+    
+    @IBAction func didClickCreateItemButton(sender: UIButton)
+    {
+        guard let image = image else { showNotEnoughDataError(); return }
+        guard let name = nameField.text where name.characters.count != 0 else { showNotEnoughDataError(); return }
+        
+        createItem(image, name: name)
+    }
+    
+    
+    func pickPhoto()
+    {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    
+    func createItem(image: UIImage, name: String)
+    {
+        server?.createItemWithName(name, image: image, success: { (item: Item) in
+            print("createItem success")
+            self.navigationController?.popViewControllerAnimated(true)
+        }, failure: { (error: NSError) in
+            print("createItem error = \(error)")
+        })
+    }
+    
+    
+    func showNotEnoughDataError()
+    {
+        let alert = UIAlertView(title: "Error", message: "Please type your name and select an image", delegate: nil, cancelButtonTitle: "OK")
+        alert.show()
+    }
+}
+
+
+extension ItemStoreCreateItemViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject])
+    {
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else { return }
+        self.image = image
+        selectedImageImageView.image = image
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController)
+    {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
